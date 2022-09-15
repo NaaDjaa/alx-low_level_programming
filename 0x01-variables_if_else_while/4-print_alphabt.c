996b5c70a5b18b1b8acd6bb51570ee5b786598ba
NaaDjaa <barnor.esther@gmail.com>
2022-09-14 20:29:41 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	char ch = 'a';
 
-	while (ch <= 'z') 
+	while (ch <= 'z')
 	{
 		if (ch != 'e' && ch != 'q')
 		{
@@ -17,6 +17,6 @@ int main(void)
 		}
 		ch++;
 	}
-  putchar('\n');
-  return (0);
+	putchar('\n');
+	return (0);
 }
