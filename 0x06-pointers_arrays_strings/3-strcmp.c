772e2fd719fcc608808ac03f40604b935a368f9b
Naadjaa <barnor.esther@gmail.com>
2022-10-20 14:58:36 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
  *   *@s1: A pointer to a character that will be changed
  *    *@s2: A pointer to a character that will also be changed/modified/updated
  *     *Return: dest
- *      */
+*/
 
 int _strcmp(char *s1, char *s2)
 
